b0bae79a359036e00b15c28e0d6e7fc19bd980f9
Vedukharatmal <vedant14052002@gmail.com>
2025-09-09 14:37:13 +0530

    minor change

--- core/settings.py
+++ core/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "assessment" / "static"]
+STATICFILES_DIRS = [BASE_DIR / "assessments" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
