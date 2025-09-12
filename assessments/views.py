80ad8b8aa7c22aad0959813f7da8ef52922a7534
Vedukharatmal <vedant14052002@gmail.com>
2025-09-12 12:39:35 +0530

    added views for signup page and dashboard page.

--- assessments/views.py
+++ assessments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Question,Response,Result,AssessmentSession,Field
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect("dashboard")
+    else:
+        form = UserCreationForm()
+    return render(request, "assessments/signup.html", {"form":form})
+
+@login_required
+def dashboard(request):
+    return render(request, "assessments/dashboard.html  ")
