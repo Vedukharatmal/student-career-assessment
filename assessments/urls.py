fc3c5c98733d88252a3f3b64d36c34f9ab3ebf77
Vedukharatmal <vedant14052002@gmail.com>
2025-09-15 16:15:10 +0530

    updated urls.py

--- assessments/urls.py
+++ assessments/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-urlpatterns = {
+urlpatterns = [
+    path("", views.landing, name="landing"),
     path("signup/", views.signup_view, name="signup"),
-    path("", views.dashboard, name="dashboard"),
-}
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("start/<int:session_id>/<str:field>/", views.question_view, name="question_view"),
+    path("start-assessment/", views.start_assessment, name="start_assessment"),
+    path("results/<int:session_id>/", views.results, name="results"),
+]
